bb146f0a15fc6471eebeaf8524a926226fff0aa9
Parag <paraggondchawar362@gmail.com>
2022-03-27 19:22:03 +0530

    week3 and week4 pushed

---
+++ week3/assignment3/march18.js
@@ -0,0 +1,55 @@
+/*************************************************************
+ * Quetion no.1 : Programm to write first 10 numbers.
+ *************************************************************/
+
+function first10numbers() {
+  for (let i = 0; i < 10; i++) {
+    console.log(i);
+  }
+}
+
+
+first10numbers();
+
+
+/**************************************************************
+ * Quetion no.2 : Program to calculate the sum of first 10 numbers
+ ***************************************************************/
+
+/***************************************************************
+ * Quetion no.4 : Write a programm to print day of week by using switch case
+ *******************************************************************/
+
+Number = ()
+ 
+   switch(Number){
+     case 0: print'sunday'
+
+     break;
+
+     case 1: print'monday'
+
+     break;
+
+     case 2: print'tuesday'
+
+     break;
+
+     case 3: print'wednesday'
+
+     break;
+
+     case 4: print'thrusday'
+
+     break;
+
+     case 5: print'friday'
+
+     break;
+
+     case 6: print'saturday'
+
+     break;
+
+     default : print 'number is invalid'
+}
